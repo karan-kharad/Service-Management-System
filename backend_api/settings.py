0131645a839fdb82f819205447d9ebec3e9b899b
Karan <karankharad93@gmail.com>
2026-04-25 11:16:31 +0530

    Update email host user in settings.py

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -188,7 +188,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "karankharad94@gmail.com"
+EMAIL_HOST_USER = "example@gmail.com"
 EMAIL_HOST_PASSWORD = "ljat hgvw vcug ztim"
 
 # 3. Allow your frontend origin
